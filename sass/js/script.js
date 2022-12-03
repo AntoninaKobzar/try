6c0f3c5e3b56dca9b88fe6dd007714e6fac13d84
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2022-12-03 23:25:57 +0200

    1

--- sass/js/script.js
+++ sass/js/script.js
@@ -1,13 +1,1 @@
-$(document).ready(function(){
-    $(".slider").slick({
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        // slidesToShow: 3,
-        // slidesToScroll:
-        // speed:300,
-        // easing:"linear",
-        // infinite: true,
-
-    }); 
-});
+new Swiper(".image-slider");
